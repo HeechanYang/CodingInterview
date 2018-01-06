d5e85f0370d971bc3e0d9092ac5c5ad17afd19ab
Yang <yhc944@gmail.com>
2018-01-06 14:29:01 +0900

    [INIT]

---
+++ src/Chapter16/Problem5.java
@@ -0,0 +1,15 @@
+package Chapter16;
+
+/**
+ * Created by YangHC on 2018-01-06.
+ */
+public class Problem5 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static int add(){
+        System.out.println("Hello");
+        return 3+4;
+    }
+}
---
+++ test/Chapter16/Problem5Test.java
@@ -0,0 +1,14 @@
+package Chapter16;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+/**
+ * Created by YangHC on 2018-01-06.
+ */
+public class Problem5Test {
+    @Test
+    public void testAdd(){
+        assertEquals("Hello World!", Problem5.add(),8);
+    }
+}
