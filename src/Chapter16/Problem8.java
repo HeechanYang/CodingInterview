0afd9e023b594b12669e55f3e9f246e1db95afbe
Yang <yhc944@gmail.com>
2018-01-08 14:56:13 +0900

    [INIT] 16-8

---
+++ src/Chapter16/Problem8.java
@@ -0,0 +1,18 @@
+package Chapter16;
+
+import java.io.*;
+
+/**
+ * Created by YangHC on 2018-01-08.
+ */
+public class Problem8 {
+    private static final String units[] = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    private static final String firstTens[] = {"Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private static final String Tens[] = {"Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private static final String block[] = {"", "Thousand", "Million", "Billion"};
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
---
+++ test/Chapter16/Problem8Test.java
@@ -0,0 +1,22 @@
+package Chapter16;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by YangHC on 2018-01-06.
+ */
+public class Problem8Test {
+    private static Problem8 p8;
+
+    @BeforeClass
+    public static void makeInstance()throws Exception {
+        p8 = new Problem8();
+    }
+
+    @Test
+    public void getMaxValueOfArray(){
+    }
+}
