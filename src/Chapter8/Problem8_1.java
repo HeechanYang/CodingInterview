7391132d86c3dab4200a72138916371be81fad1c
Yang <yhc944@gmail.com>
2018-01-10 14:46:33 +0900

    [COMPLETE] 8-1

--- src/Chapter8/Problem8_1.java
+++ src/Chapter8/Problem8_1.java
@@ -1,14 +1,50 @@
 package Chapter8;
 
+import java.util.Arrays;
+
 /**
  * Created by YangHC on 2018-01-10.
  */
 public class Problem8_1 {
-    private int ways[];
+    private int waysCount[] = new int[99999];
 
-    public static void main(String[] args) {
-
+    public Problem8_1() {
+        Arrays.fill(waysCount, -1);
+        waysCount[0] = 1;
+        waysCount[1] = 2;
+        waysCount[2] = 4;
     }
 
-//    public static int
+    public static void main(String[] args) {
+        Problem8_1 p1 = new Problem8_1();
+
+//        int n = Integer.parseInt(args[0]);
+        System.out.println(p1.getCountWays(4));
+    }
+
+    public int getCountWays(int n) {
+        int n1, n2, n3;
+        if (n == 1) {
+            return waysCount[0];
+        } else if (n == 2) {
+            return waysCount[1];
+        } else if (n == 3) {
+            return waysCount[2];
+        } else {
+            if (waysCount[n - 2] == -1) {
+                waysCount[n - 2] = getCountWays(n - 1);
+            }
+            if (waysCount[n - 3] == -1) {
+                waysCount[n - 3] = getCountWays(n - 2);
+            }
+            if (waysCount[n - 4] == -1) {
+                waysCount[n - 4] = getCountWays(n - 3);
+            }
+            n1 = waysCount[n - 2];
+            n2 = waysCount[n - 3];
+            n3 = waysCount[n - 4];
+
+            return n1 + n2 + n3;
+        }
+    }
 }
--- test/Chapter8/Problem8_1Test.java
+++ test/Chapter8/Problem8_1Test.java
@@ -17,8 +17,16 @@ public class Problem8_1Test {
     }
 
     @Test
-    public void testFindLastZeros(){
-//        assertEquals(3, );
+    public void testCountWays(){
+        assertEquals(1, p1.getCountWays(1));
+        assertEquals(2, p1.getCountWays(2));
+        assertEquals(4, p1.getCountWays(3));
+        assertEquals(7, p1.getCountWays(4));
+        assertEquals(13, p1.getCountWays(5));
+        assertEquals(24, p1.getCountWays(6));
+        assertEquals(44, p1.getCountWays(7));
+        assertEquals(81, p1.getCountWays(8));
+        assertEquals(149, p1.getCountWays(9));
     }
 
 }
