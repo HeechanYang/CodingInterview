41523221901dc726dcfc053fbb49639aa5f5b024
Yang <yhc944@gmail.com>
2018-01-12 13:35:59 +0900

    [INIT] 10-5

---
+++ src/Chapter10/Problem10_5.java
@@ -0,0 +1,16 @@
+package Chapter10;
+
+/**
+ * Created by YangHC on 2018-01-12.
+ *
+ * [문제 10-5]
+ * 드문드문 탐색 ; 빈 문자열이 섞여 있는 정렬된 문자열 배열이 주어졌을 때,
+ * 특정 문자열의 위치를 차즌 메서드를 작성하라.
+ *
+ *
+ * [Sort], [Search]
+ */
+public class Problem10_5 {
+    private String arr[] = {"at","","","","ball","","","car","","","dad","",""};
+    private String arr2[];
+}
---
+++ test/Chapter10/Problem10_5Test.java
@@ -0,0 +1,17 @@
+package Chapter10;
+
+import org.junit.BeforeClass;
+
+/**
+ * Created by YangHC on 2018-01-12.
+ */
+public class Problem10_5Test {
+    private static Problem10_5 p5;
+
+    @BeforeClass
+    public static void makeInstance() throws Exception {
+        p5 = new Problem10_5();
+    }
+
+//    @Test
+}
